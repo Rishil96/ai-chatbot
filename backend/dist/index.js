d9fd83acf6524761c4aa89b8f9781fda5f43b5e6
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-02 18:17:45 +0530

    Add: Basic GET route for backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,8 @@
-console.log("This is a starter kit for this amazing project. With 💓 By Indian Coders");
-export {};
+import express from 'express';
+const app = express();
+// Basic GET Route
+app.get("/", (req, res, next) => {
+    return res.send("Welcome to initial phase of ChatBot Application powered by GeminiAI");
+});
+app.listen(5000, () => console.log("Server is listening on port 5000"));
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,10 @@
-console.log(
-  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
-);
+import express from 'express';
+
+const app = express();
+
+// Basic GET Route
+app.get("/", (req, res, next) => {
+  return res.send("Welcome to initial phase of ChatBot Application powered by GeminiAI");
+})
+
+app.listen(5000, () => console.log("Server is listening on port 5000"));
