c22b5e6e9ba6fd0ef6b75501883e6988e2e1719d
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-02 18:27:16 +0530

    Add : POST and Dynamic URL Route in our basic express app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,26 @@ import express from 'express';
 
 const app = express();
 
+// Add middleware to use json in our express application
+app.use(express.json());
+
+
 // Basic GET Route
 app.get("/", (req, res, next) => {
   return res.send("Welcome to initial phase of ChatBot Application powered by GeminiAI");
 })
+
+
+// Greet POST Route
+app.post("/greet", (req, res, next) => {
+  return res.json({"message": `Hey ${req.body.name}! How you doing?`})
+})
+
+
+// Dynamic Routing
+app.post("/user/:id", (req, res, next) => {
+  return res.json({"message": `User ${req.params.id}. How may I help you?`});
+})
+
 
 app.listen(5000, () => console.log("Server is listening on port 5000"));
